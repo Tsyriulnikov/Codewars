f1be882daa87bc24b365e8279c377791929758ad
Tsyriulnikov <max_djvu@mail.ru>
2022-02-12 22:39:09 +0300

    third day

---
+++ issue08.js
@@ -0,0 +1,46 @@
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input array is empty or null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0)
+      return [];
+    
+    var positive = 0;
+    var negative = 0;
+    
+    for (var i=0, l=input.length; i<l; ++i)
+    {
+      if (input[i] > 0)
+        ++ positive;
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
+}
+
+или
+
+
+function countPositivesSumNegatives(input) {
+    return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
+
+или
+
+function countPositivesSumNegatives(input) {
+    if(input === null || input.length < 1) return [];
+    let r = [0,0];
+    input.forEach(function(e) {
+      (e>0) ? r[0]++ : r[1] += e;
+    });
+    return r;
+}
