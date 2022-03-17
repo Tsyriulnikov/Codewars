424c8ea389bc9dfcd1e904a8bd8ecf600a7be1f6
Tsyriulnikov <max_djvu@mail.ru>
2022-03-17 20:36:48 +0300

    17_03_22

---
+++ issue61.js
@@ -0,0 +1,37 @@
+/*
+In the following 6 digit number:
+
+283910
+91 is the greatest sequence of 2 consecutive digits.
+
+In the following 10 digit number:
+
+1234567890
+67890 is the greatest sequence of 5 consecutive digits.
+
+Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+*/
+
+function solution(digits){
+    let arr = [...'' + digits].map(Number)
+    let result=0
+    let number=0
+    for (let i=0; i<arr.length-4;i++){
+     number=arr.slice(i,i+5).join('')
+    if (number>result) {result = number} 
+    }
+    return +result
+    }
+
+    или 
+    function solution(digits){
+        let s = [];
+        for (var i = 0; i < digits.length - 4; i++){
+            s.push(digits.substr(i,5));
+        };
+        return Math.max(...s);
+    }
+    
+    или
+    const solution = digits =>
+  [...digits].reduce((pre, _, idx) => Math.max(pre, digits.slice(idx, idx + 5)));
