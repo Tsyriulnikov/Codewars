bac3158cf0db0983bb28bc42be605903cf9397ac
Tsyriulnikov <max_djvu@mail.ru>
2022-05-19 08:45:16 +0300

    issue72

---
+++ issue72.js
@@ -0,0 +1,24 @@
+/*
+Don't give me five!
+In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+Examples:
+
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+The result may contain fives. ;-)
+The start number will always be smaller than the end number. Both numbers can be also negative!
+*/
+
+const dontGiveMeFive=(start, end)=> {
+    let result=0
+    for(let i=start;i<=end;i++){
+    if (!i.toString().includes("5") ) result=result+1
+    }
+    return result
+    
+    }
+
+
+    или
+    const dontGiveMeFive=(s,e)=>[...Array(e-s+1)].reduce((r,_,n)=>r+!/5/.test(n+s),0)
